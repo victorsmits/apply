38b195a8bd6d01b73523dca7504bfcb0c3d37d1c
victorsmits <victor.smits@student.vinci.be>
2018-12-05 09:57:42 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/apply.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/apply.dir/main.c.o"
+  "apply.pdb"
+  "apply"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/apply.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
