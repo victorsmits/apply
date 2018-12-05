745d47f9b4d11872f541fb0e045e701bc260fe82
victorsmits <victor.smits@student.vinci.be>
2018-12-05 10:32:33 +0100

    up

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+int apply(char *str, bool (*check) (char));
+bool check(char def);
+int size(char *s);
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    printf("%d\n",apply("AAABBBaaabbb", check));
+}
+
+int size(char *s){
+    int size = 0;
+    while (*s ++ != '\0')
+        size ++;
+    return size ;
+}
+
+bool check(char def){
+    if (def == 'A')
+        return true;
+    else
+        return false;
+}
+
+int apply(char *str, bool (*check) (char)){
+    int N = size(str)+1;
+    int i;
+    int count = 0;
+    for(i = 0; i< N; i++){
+        if(check(str[i])){
+            count ++;
+        }
+    }
+    return count;
 }
